6b5d2c472ac4a6dd5c05738d79daf39804b6fbbf
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-27 19:06:58 -0500

    Completed steps 1-5

---
+++ script.js
@@ -0,0 +1,35 @@
+//Current Weather POJO
+let enterpriseWeather = {
+  latitude: 31.325638,
+  longitude: -85.846725,
+  generationtime_ms: 0.14209747314453125,
+  utc_offset_seconds: -18000,
+  timezone: "America/Chicago",
+  timezone_abbreviation: "GMT-5",
+  elevation: 108.0,
+  current_units: {
+    time: "iso8601",
+    interval: "seconds",
+    temperature_2m: "°F",
+    precipitation: "inch",
+  },
+  current: {
+    time: "2025-08-27T18:45",
+    interval: 900,
+    temperature_2m: 79.3,
+    precipitation: 0.0,
+    wind_speed_10m: 5.9,
+  },
+  daily_units: {
+    time: "iso8601",
+    temperature_2m_max: "°F",
+    temperature_2m_min: "°F",
+    weather_code: "wmo code",
+  },
+  daily: {
+    time: ["2025-08-27"],
+    temperature_2m_max: [86.3],
+    temperature_2m_min: [67.2],
+    weather_code: [3],
+  },
+};
